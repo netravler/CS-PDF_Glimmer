420c7bf4f346b3cb2f2108531ccda500e5ab659d
netravler <pheintz@hotmail.com>
2017-04-28 13:29:41 -0500

    Add files via upload

---
+++ PDF_Glimmer/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PDF_Glimmer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblPickAFile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 428);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(809, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(41, 123);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1065, 285);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 66);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1065, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Click Me for A File Picker";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // lblPickAFile
+            // 
+            this.lblPickAFile.AutoSize = true;
+            this.lblPickAFile.Location = new System.Drawing.Point(37, 47);
+            this.lblPickAFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPickAFile.Name = "lblPickAFile";
+            this.lblPickAFile.Size = new System.Drawing.Size(73, 17);
+            this.lblPickAFile.TabIndex = 5;
+            this.lblPickAFile.Text = "Pick A File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 486);
+            this.Controls.Add(this.lblPickAFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "PDF_Glimmer - Demo App Might work:)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblPickAFile;
+    }
+}
+
---
+++ PDF_Glimmer/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+using org.pdfbox.pdmodel;
+using org.pdfbox.util;
+using org.pdfbox.examples.pdmodel;
+using org.pdfbox.exceptions;
+using org.pdfbox.pdmodel.documentinterchange;
+using org.pdfbox.pdmodel.interactive.documentnavigation.outline;
+using org.pdfbox.pdmodel.interactive.documentnavigation.destination;
+using org.pdfbox.pdfparser;
+using org.pdfbox.pdmodel.encryption;
+using org.pdfbox.pdfwriter;
+using org.pdfbox.cos;
+using org.pdfbox.pdmodel.font;
+
+using java.lang;
+using java.io;
+using java.util;
+
+using IKVM.Attributes;
+using IKVM.Runtime;
+
+
+
+namespace PDF_Glimmer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+        public string parsePDF(string pdf_in, string txt_out)
+        {
+            //StreamWriter sw = new StreamWriter(txt_out, false);
+            string txt;
+            try
+            {
+                //sw.WriteLine();
+                //sw.WriteLine(DateTime.Now.ToString());
+                PDDocument doc = PDDocument.load(pdf_in); 
+                PDFTextStripper stripper = new PDFTextStripper(); 
+                //sw.Write(stripper.getText(doc));
+                txt = stripper.getText(doc);
+                return txt;
+            } 
+            finally
+            {
+                //sw.Close();
+               // sw.Dispose();
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = parsePDF(@textBox1.Text, @"c:\Leads_out.txt");
+            }
+            catch
+            {
+                richTextBox1.Text = "Noppers there was a problem";
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
